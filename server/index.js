627469359eb5b94dcded3870f6ab628822b30c14
rohan <khem7869218659@gmail.com>
2024-02-24 14:27:39 +0530

    last commit rearrange the configration

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const postRoutes = require('./routes/posts');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -13,10 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL ='mongodb+srv://Rohan786:RohanMehra@cluster0.921idca.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 8000;
+// const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
